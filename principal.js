7c1489bc7627b2d596a94e08818c99dd54d7b1eb
AnaJuliaFomento <146388084+AnaJuliaFomento@users.noreply.github.com>
2024-02-23 17:28:55 -0300

    Add files via upload
    Ana Júlia
    Joana

--- principal.js
+++ principal.js
@@ -3,4 +3,12 @@ let calcula = document.getElementById('calcula')
 
 calcula.addEventListener('click',()=> {
     
+
+     const volume = new Volume()
+
+     let volumePrisma = volume.vPrisma()
+     let volumePiramide = volume.vPiramide()
+     let volumeCilindro = volume.vCilindro()
+     let volumeCone = volume.vCone()
+     let volumeEsfera = volume.vEsfera()
 })
